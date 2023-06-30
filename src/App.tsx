df84e62b405a38f1114aaf31e61f1d9dc8c799fc
Gutembergue Junior <gutemsrocha@gmail.com>
2023-06-30 17:24:43 -0300

    Product-details dinamyc routes created

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
         <Route path="/" element={<ClientHome />}>
           <Route path="index" element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details" element={<ProductDetails />} />
+          <Route path="product-details/:productId" element={<ProductDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
