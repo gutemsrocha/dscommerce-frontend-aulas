e474287d0ae5052f77cfd44f81fe9a0daad74d0c
Gutembergue Junior <gutemsrocha@gmail.com>
2023-06-30 18:04:35 -0300

    Links

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import Catalog from './routes/ClientHome/Catalog';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import ClientHome from './routes/ClientHome';
 
-
-
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClientHome />}>
-          <Route path="index" element={<Catalog />} />
+          <Route index element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
           <Route path="product-details/:productId" element={<ProductDetails />} />
         </Route>
